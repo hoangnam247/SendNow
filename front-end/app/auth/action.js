fa81c188bacb292d6e4d14eade14b583ff14a374
Tui-Ten-Nam <namhoang.240102@gmail.com>
2025-03-26 23:19:12 +0700

    cookie

--- front-end/app/auth/action.js
+++ front-end/app/auth/action.js
@@ -1,3 +1,8 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 export const handleLogin = async (formData) => {
     try {
       const form = Object.fromEntries(formData);
